3042f527789559051e4efef879ff06042fb9c108
Avadh <dev.avadhksapariya@gmail.com>
2024-09-24 18:54:32 +0530

    CounterApp - Bloc Learning                - Created new project and initial setup with uploding on remote.

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:counter_app/screen_home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+/*
+
+1. bloc
+  - The bloc package contains the things that we use in our bloc layer. i.e. bloc class
+  - This isn't necessarily flutter dependent.
+  - It is just the logic architecture of the flutter app.
+  - Can be used in the dart apps only.
+
+2. flutter_bloc
+  - The flutter bloc contains elements that we can use in our UI layer.
+  - It includes widgets like bloc provide, bloc builder etc.
+
+>> Two approaches while managing state and bloc
+  i. By using bloc state management
+  ii. By using cubit
+
+ */
